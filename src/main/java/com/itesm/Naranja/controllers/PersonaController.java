12286dad7a80af284168919f301786d365f680b6
DiegoManzanarezx <manxana.diego@gmail.com>
2022-04-01 20:10:25 -0600

    update

--- src/main/java/com/itesm/Naranja/controllers/PersonaController.java
+++ src/main/java/com/itesm/Naranja/controllers/PersonaController.java
@@ -1,19 +1,35 @@
 package com.itesm.Naranja.controllers;
-import com.itesm.Naranja.models.Persona;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.itesm.Naranja.models.Persona;
+import com.itesm.Naranja.services.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/persona")
 public class PersonaController {
 
+    @Autowired
+    PersonaService personaService;
+
     @GetMapping("/perfil")
     public Persona getPerfil(){
-        Persona p = new Persona();
-        p.setNombre("Diego");
+        Persona p= new Persona();
+        p.setNombre("Andrés");
         p.setEdad(32);
         return p;
+    }
+
+    @GetMapping("/all")
+    public List<Persona> getAll(){
+        return personaService.getAll();
+    }
+
+    @PostMapping("/save")
+    public Persona save(@RequestBody Persona persona) throws Exception{
+        personaService.insertar(persona);
+        return persona;
     }
 }
