882edb6d7f568b6f3e8c6a8591cb3c04ff8518c2
DiegoManzanarezx <manxana.diego@gmail.com>
2022-04-02 17:08:14 -0600

    save3

--- src/main/java/com/itesm/Naranja/controllers/SupervisorController.java
+++ src/main/java/com/itesm/Naranja/controllers/SupervisorController.java
@@ -52,7 +52,7 @@ public class SupervisorController {
             produces = "application/json"
     )
     @ResponseBody
-    public Supervisor save(Supervisor supervisor){
+    public Supervisor save(@RequestBody Supervisor supervisor){
         supervisorService.insertSupervisor(supervisor);
         System.out.println(supervisor.toString());
         return supervisor;
